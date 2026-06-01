69aa9fa909354117c13f293cac94a1206bd1149e
Olietho Godfrey Josiah <jj.o.godfrey@gmail.com>
2026-06-01 22:21:02 +0300

    Add loginUser function to authApi

--- frontend/aits_frontend/src/api/authApi.js
+++ frontend/aits_frontend/src/api/authApi.js
@@ -1,1 +1,8 @@
+import apiRequest from "./apiClient";
 
+export function loginUser(data) {
+  return apiRequest("/users/auth/login/", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+}
